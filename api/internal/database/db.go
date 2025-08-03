dcff592103e0d9228cb75b47750d568aba122155
hugocbb <hugocarlosb.071@gmail.com>
2025-08-02 21:57:28 -0300

    chore: "Adicionado dockerizacao de api"

--- api/internal/database/db.go
+++ api/internal/database/db.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/crm/api/config"
 	"github.com/crm/api/internal/domain"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -27,13 +26,18 @@ func ConnectDatabase() error {
 }
 
 func openDatabase() (*gorm.DB, error) {
-	cfg := config.NewDatabaseConfig()
+	// cfg := config.NewDatabaseCosnfig()
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		cfg.Host,
-		cfg.User,
-		cfg.Password,
-		cfg.Name,
-		cfg.Port,
+		// cfg.Host,
+		// cfg.User,
+		// cfg.Password,
+		// cfg.Name,
+		// cfg.Port,
+		"db",
+		"postgres",
+		"root",
+		"crm",
+		"5432",
 	)
 
 	DB, err = gorm.Open(postgres.Open(dsn))
