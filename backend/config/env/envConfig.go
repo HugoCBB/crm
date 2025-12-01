45d74e9b333cb2c797e8d139bfa580cb192af77e
Hugo <157296697+HugoCBB@users.noreply.github.com>
2025-12-01 13:39:08 -0300

    Load specific .env file in LoadEnv function

--- backend/config/env/envConfig.go
+++ backend/config/env/envConfig.go
@@ -7,7 +7,7 @@ import (
 )
 
 func LoadEnv() error {
-	if err := godotenv.Load(); err != nil {
+	if err := godotenv.Load(".env"); err != nil {
 		log.Fatal("Erro ao procurar arquivo .env")
 		return err
 	}
